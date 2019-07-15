4ab5668a6804d9fd716738e60c42b884bc141f53
Dalton Lima <dl@seemore.lu>
2019-07-15 19:12:39 +0200

    add vec3 and ray classes

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "vec3.h"
 
 // Generating the content of an image file in .ppm format
 
@@ -12,12 +13,11 @@ int main()
     {
         for (int i = 0; i < nx; i++)
         {
-            float r = float(i) / float (nx);
-            float g = float(j) / float (ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int (255.99 * g);
-            int ib = int (255.99 * b);
+            vec3 col(float(i) / float (nx), float(j) / float (ny), 0.2);
+            int ir = int (255.99 * col[0]);
+            int ig = int (255.99 * col[1]);
+            int ib = int (255.99 * col[2]);
+            
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
