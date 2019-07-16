c984a5f93af0ffbd22d702dde681df3a01756766
Dalton Lima <dl@seemore.lu>
2019-07-16 19:45:19 +0200

    chapter 5 - hitable and hitable_list

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
-#include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r)
+vec3 color(const ray& r, hitable *world)
 {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4 * a * c;
-    
-    if (discriminant < 0)
+    hit_record rec;
+    if (world->hit(r, 0.0, MAXFLOAT, rec))
     {
-        return -1.0;
+        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
     }
     else
     {
-        return (-b - sqrt(discriminant)) * (2.0 * a);
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        // Lerping from white to blue in y
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
     }
-}
-
-vec3 color(const ray& r)
-{
-    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-    if (t > 0.0)
-    {
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
-    // Lerping from white to blue in y
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 // Generating the content of an image file in .ppm format
@@ -46,6 +32,12 @@ int main()
     vec3        vertical  ( 0.0,  2.0,  0.0);
     vec3        origin    ( 0.0,  0.0,  0.0);
 
+    hitable *list[3];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    // A giant sphere to act as a ground (or a planet)
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable *world = new hitable_list(list, 2);
+
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
@@ -54,7 +46,8 @@ int main()
             float v = float(j) / float(ny);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
             
-            vec3 col = color(r);
+            vec3 p = r.point_at_parameter(2.0);
+            vec3 col = color(r, world);
             int ir = int (255.99 * col[0]);
             int ig = int (255.99 * col[1]);
             int ib = int (255.99 * col[2]);
