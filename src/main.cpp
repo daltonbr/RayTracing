ecac33a4d35e3fb528cfa8b1105b67cbc421546e
Dalton Lima <dl@seemore.lu>
2019-07-16 09:02:45 +0200

    rendering sphere with normals

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0 * dot(oc, r.direction());
     float c = dot(oc, oc) - radius * radius;
     float discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+    
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) * (2.0 * a);
+    }
 }
 
 vec3 color(const ray& r)
 {
-    if (hit_sphere(vec3(0, 0, -1), 0.5f, r))
+    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
     {
-        return vec3(1, 0, 0);
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     // Lerping from white to blue in y
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
