4a8e6bb055b4e007ce939f48eb55190193bba36a
Dalton Lima <dl@seemore.lu>
2019-07-19 08:04:14 +0200

    Finish Chapter 6 - Antialising

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 
 // Image converting
 #define STB_IMAGE_IMPLEMENTATION
@@ -42,29 +43,31 @@ int main()
 
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3        horizontal( 4.0,  0.0,  0.0);
-    vec3        vertical  ( 0.0,  2.0,  0.0);
-    vec3        origin    ( 0.0,  0.0,  0.0);
-
-    hitable *list[3];
+    hitable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     // A giant sphere to act as a ground (or a planet)
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
     hitable *world = new hitable_list(list, 2);
 
+    camera cam;
+
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            
-            vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++)
+            {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
+            col /= float(ns);
             int ir = int (255.99 * col[0]);
             int ig = int (255.99 * col[1]);
             int ib = int (255.99 * col[2]);
